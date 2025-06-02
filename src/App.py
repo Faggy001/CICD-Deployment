e99b8933becaa3498b2f535a1e316dbdba234d13
faggy001 <faggy29@gmail.com>
2025-06-02 14:08:11 +0100

    MODIFIED App.py

--- src/App.py
+++ src/App.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello Xapic from Group-B Flask on AWS!"
+@app.route('/')
+def hello_docker():
+    return '<h1> This is Xapic Group-B</h1><br><p>Thank you for reading. We are the best.</p> '
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
